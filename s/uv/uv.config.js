12322ecb36ad5f7852f6a88e51e6094d800d2cac
Oscar Su <nexoraunblocked@gmail.com>
2025-11-13 05:53:28 -0600

    made proxy actually work

--- s/uv/uv.config.js
+++ s/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: "/s/ultraviolet/",
-	bare: "https://meximathurbanned.global.ssl.fastly.net/tspmo/",
+	bare: "https://3sow4six3pqlu5t4qgyximuyrm0nlegb.lambda-url.us-east-2.on.aws/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/s/uv/uv.handler.js",
